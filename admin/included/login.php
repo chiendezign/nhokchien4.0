855f935cda6f28dbbbde30d83514ed2ac2aa3014
Nhok chien <boymituot@gmail.com>
2016-02-03 09:53:45 +0700

    Update code

--- admin/included/login.php
+++ admin/included/login.php
@@ -8,46 +8,28 @@
             <div class="panel-heading">
                 <div class="panel-title">Trang đăng nhập</div>
             </div>
-
             <div style="padding-top:30px" class="panel-body">
-
                 <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-
                 <form id="loginform" class="form-horizontal" role="form">
-
                     <div style="margin-bottom: 25px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input id="login-username" type="text" class="form-control" name="username" value=""
-                               placeholder="Tên đăng nhập">
+                        <input id="username" type="text" class="form-control" name="username" value="" placeholder="Tên đăng nhập">
                     </div>
-
                     <div style="margin-bottom: 25px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                        <input id="login-password" type="password" class="form-control" name="password"
-                               placeholder="Mật khẩu">
+                        <input id="password" type="password" class="form-control" name="password"  placeholder="Mật khẩu">
                     </div>
-
-
                     <div class="input-group">
                         <div class="checkbox">
-                            <label>
-                                <input id="login-remember" type="checkbox" name="remember" value="1"> Ghi nhớ thông tin
-                            </label>
+                            <label><input id="login-remember" type="checkbox" name="remember" value="1"> Ghi nhớ thông tin</label>
                         </div>
                     </div>
-
-
                     <div style="margin-top:10px" class="form-group">
-                        <!-- Button -->
-
                         <div class="col-sm-12 controls">
                             <a id="btn-login" href="#" class="btn btn-success">Đăng nhập </a>
                             <a id="btn-fblogin" href="#" class="btn btn-primary">Về trang chủ</a>
-
                         </div>
                     </div>
-
-
                     <div class="form-group">
                         <div class="col-md-12 control">
                             <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%">
@@ -57,24 +39,8 @@
                         </div>
                     </div>
                 </form>
-
-
             </div>
         </div>
-    </div>
-    <div id="signupbox" style="display:none; margin-top:50px"
-         class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                <div class="panel-title">Sign Up</div>
-                <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="#"
-                                                                                           onclick="$('#signupbox').hide(); $('#loginbox').show()">Sign
-                        In</a></div>
-            </div>
-
-        </div>
-
-
     </div>
 </div>
 </body>
---
+++ admin/included/route.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nhok
+ * Date: 2/3/2016
+ * Time: 9:46 AM
+ */
