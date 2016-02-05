fec8213598fda3b6d2be57b5a1a8bacc0c5f5b97
Nhok chien <boymituot@gmail.com>
2016-02-05 17:36:58 +0700

    Update code

--- admin/included/login.php
+++ admin/included/login.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="vi">
-<?php include 'modules/head.php' ?>
+<?php include 'modules/head.php'?>
 <body style="background: #FFF">
 <div class="container">
-    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-2">
         <div class="panel panel-info">
             <div class="panel-heading">
                 <div class="panel-title">Trang đăng nhập</div>
             </div>
-            <div style="padding-top:30px" class="panel-body">
+            <div style="padding-top:10px" class="panel-body">
                 <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
                 <form id="loginform" class="form-horizontal" role="form">
-                    <div style="margin-bottom: 25px" class="input-group">
+                    <div style="margin-bottom: 10px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                         <input id="username" type="text" class="form-control" name="username" value="" placeholder="Tên đăng nhập">
                     </div>
-                    <div style="margin-bottom: 25px" class="input-group">
+                    <div style="margin-bottom: 5px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
                         <input id="password" type="password" class="form-control" name="password"  placeholder="Mật khẩu">
                     </div>
@@ -24,20 +24,13 @@
                             <label><input id="login-remember" type="checkbox" name="remember" value="1"> Ghi nhớ thông tin</label>
                         </div>
                     </div>
-                    <div style="margin-top:10px" class="form-group">
+                    <div style="margin-top:5px; margin-bottom: 0" class="form-group">
                         <div class="col-sm-12 controls">
                             <a id="btn-login" href="#" class="btn btn-success">Đăng nhập </a>
                             <a id="btn-fblogin" href="#" class="btn btn-primary">Về trang chủ</a>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <div class="col-md-12 control">
-                            <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%">
-                                Đây là trang dành riêng cho BQT. Nếu bạn không phải là thành viên quản trị, vui lòng trở
-                                lại trang chủ
-                            </div>
-                        </div>
-                    </div>
+
                 </form>
             </div>
         </div>
