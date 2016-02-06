d1a13649ada209ed8bfebd9db5f0ed99018ddade
Nhok chien <boymituot@gmail.com>
2016-02-06 22:45:14 +0700

    login

--- admin/included/login.php
+++ admin/included/login.php
@@ -3,19 +3,19 @@
 <?php include 'modules/head.php' ?>
 <body style="background: #FFF">
 <div class="container">
-    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+    <div id="loginbox" style="margin-top:50px;" class="col-md-4 col-md-offset-4 col-sm-8 col-sm-offset-2">
         <div class="panel panel-info">
             <div class="panel-heading">
                 <div class="panel-title">Trang đăng nhập</div>
             </div>
-            <div style="padding-top:30px" class="panel-body">
+            <div style="padding-top:10px" class="panel-body">
                 <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
                 <form id="loginform" class="form-horizontal" role="form">
-                    <div style="margin-bottom: 25px" class="input-group">
+                    <div style="margin-bottom: 10px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
                         <input id="username" type="text" class="form-control" name="username" value="" placeholder="Tên đăng nhập">
                     </div>
-                    <div style="margin-bottom: 25px" class="input-group">
+                    <div style="margin-bottom: 10px" class="input-group">
                         <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
                         <input id="password" type="password" class="form-control" name="password"  placeholder="Mật khẩu">
                     </div>
@@ -30,7 +30,7 @@
                             <a id="btn-fblogin" href="#" class="btn btn-primary">Về trang chủ</a>
                         </div>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group" style="margin-bottom: 0">
                         <div class="col-md-12 control">
                             <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%">
                                 Đây là trang dành riêng cho BQT. Nếu bạn không phải là thành viên quản trị, vui lòng trở
